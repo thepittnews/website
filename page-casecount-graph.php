68010a2477e0c08f09f293a284016e883317512f
Jon Moss <me@jonathanmoss.me>
2021-07-26 11:48:24 -0400

    Remove dead code

--- page-casecount-graph.php
+++ page-casecount-graph.php
@@ -13,17 +13,11 @@ $csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTwWCQl2QwJyfaIBM3x99
 $fileContents = file_get_contents($csvUrl);
 $rows = array_map("str_getcsv", explode("\r\n", $fileContents));
 array_shift($rows);
-//array_shift($rows);
 
 $dates_arr = array('');
-//$pitt_students_arr = array();
-//$pitt_employees_arr = array();
-//$residents_15213_arr = array();
 
 // Transform CSV data into arrays
 for($i = 1; $i <= count($rows); $i++) {
-//foreach($rows as $row) {
-  //if ($i == 0) continue;
   if (strpos($rows[$i][0], '2020') !== false) {
     $dates_arr[] = explode("/2020", $rows[$i][0])[0] . "/20";
   }
@@ -31,24 +25,6 @@ for($i = 1; $i <= count($rows); $i++) {
   if (strpos($rows[$i][0], '2021') !== false) {
     $dates_arr[] =  explode("/2021", $rows[$i][0])[0] . "/21";
   }
-
-  //if (empty($row[1])) {
-    //$pitt_students_arr[] = null;
-  //} else {
-    //$pitt_students_arr[] = $row[1];
-  //}
-
-  //if (empty($row[2])) {
-    //$pitt_employees_arr[] = null;
-  //} else {
-    //$pitt_employees_arr[] = $row[2];
-  //}
-
-  //if (empty($row[7])) {
-    //$residents_15213_arr[] = null;
-  //} else {
-    //$residents_15213_arr[] = $row[7];
-  //}
 }
 
 // Stop axes where data ends
@@ -56,10 +32,6 @@ $stop_index = array_search(date('n/j/y'), $dates_arr, true);
 
 $dates_arr = array_slice($dates_arr, 0, $stop_index + 1, true);
 $rows = array_slice($rows, 0, $stop_index + 1, true);
-//$dates_arr = array_slice($dates_arr, 0, $stop_index + 1, true);
-//$pitt_students_arr = array_slice($pitt_students_arr, 0, $stop_index + 1, true);
-//$pitt_employees_arr = array_slice($pitt_employees_arr, 0, $stop_index + 1, true);
-//$residents_15213_arr = array_slice($residents_15213_arr, 0, $stop_index + 1, true);
 
 $out = fopen('php://output', 'w');
 fputcsv($out, $dates_arr);
