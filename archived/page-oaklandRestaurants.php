d375cbf67dfd54c1a06108a34524b6f6265ee928
Jon Moss <me@jonathanmoss.me>
2021-08-11 11:37:39 -0400

    Add archived oakland restaurants template

---
+++ archived/page-oaklandRestaurants.php
@@ -0,0 +1,109 @@
+<?php /* Template Name: Oakland Restaurants */ ?>
+
+<!DOCTYPE html>
+
+<head>
+
+	<style>
+		.p1 {
+			font-size:18px;
+			font-family: 'Roboto';
+			margin:10px 0px;
+		}
+
+		.templateTitle {
+			font-family:Lato !important;
+			font-size:36px;
+			font-weight:bold;
+			margin:25px 0px;
+		}
+	</style>
+
+	<?php include 'header.php'; ?>
+
+<span id="author-info" class="meta-info"><p class="templateTitle">Josh Ye</p></span>
+
+<p class="p1">Where the last few decals of the old Pittsburgh Pretzel Sandwich Shop on Forbes Avenue used to stick, a poster of an energetic Chinese chef, smiling as he hand-pulls noodles, now brightens the dark window.</p>
+<p class="p1">In bold Chinese characters, the poster announces, “Lanzhou Lamian Asian Noodle Bar Coming Soon!”</p>
+<p class="p1">In the last five years, the burgeoning market for Asian restaurants in Oakland has filled storefronts and windows, becoming a booming mini-economy thriving in one of Pittsburgh’s busiest neighborhoods. Today, there are 20 Asian restaurants and three Asian food trucks within just a half-mile radius of the Cathedral of Learning.</p>
+<p class="p1">In particular, on Oakland Avenue, Asian restaurants fill five out of the seven eateries on the block between Fifth and Forbes Avenues.</p>
+<p class="p1">The increase in Asian cuisine in Oakland follows an uptick in students here who identify as Pacific Islander or Asian studying at Pitt.</p>
+<p class="p1">In 2011, for example, 1,408 undergraduate and graduate students at Pitt identified as Pacific Islander or Asian-American. Now, that number has reached 2,188 students. Pitt draws these numbers from the boxes students can choose to check off when applying to Pitt, according to Alyssa Slazinski from Pitt’s Office of Institutional Research.</p>
+<p class="p1">With new eateries and Asian fusion cafes opening in Oakland almost monthly, the need to attract both Asian-American and non-Asian students drives competition between restaurant owners. But in the tight, 1.5 square mile neighborhood flowing with spending money, success isn’t a guarantee.</p>
+<p class="templateTitle"><strong>The Market</strong></p>
+<p class="p1">According to Ja-way Wang, a junior marketing major and vice president of Asian Students Alliance, Asian students are getting pickier about their food when they eat out.</p>
+<p class="p1">“I think there are a decent number of affordable restaurants in Oakland,” he said. “[But] as somebody who can cook Chinese food, I will only go to a restaurant if they have dishes I cannot cook myself and of quality that surpasses my own cooking.”</p>
+<p class="p1">The 2,404 Chinese international students at Pitt account for more than 50 percent of the University’s total international students. In the summer and fall of 2015 alone, Carnegie Mellon University admitted 1,101 students from China.</p>
+<p class="p1">Asian restaurant owners in Oakland recognize that Chinese international students go out to find a bit of home inside authentic menu items.</p>
+<p class="p1">Szechuan Express serves more than 200 Americanized Asian dishes, ranging from General Tso to Pad Thai. Most of its dishes are Asian fast food at $6 per dish.</p>
+<p class="p1">Zhilong Zheng, the cashier at Szechuan Express and member of the Lin family, said the family’s restaurant aims to provide an “affordable” and “simplified version” of authentic Chinese dishes.</p>
+<p class="p1">But to keep up with a growing Chinese population, Szechuan extended its menu in 2014, to include more authentic dishes like beef short ribs with lamb flavor.</p>
+<p class="p1">Kathleen Musante, the chair of Pitt’s anthropology department who specializes in the anthropology of food, said chefs and restaurant owners sell the idea of authenticity to bring in more customers.</p>
+<p class="p1">“I have come to believe that the only way that you can understand authenticity is as a marketing tool because cuisine is constantly changing,” Musante said. “So the narrative of authenticity carries ideas, values and identities with [it].”</p>
+<p class="p1">Sichuan Gourmet — which opened its second restaurant in 2015, on Atwood Street, filling a hole left by an Indian restaurant — has a menu that exclusively prides itself on authentic Chinese dishes such as fish filets in hot chili oil and spicy beef in flaming pan.</p>
+
+<img class="wp-image-65693 size-full" src="https://pittnews.com/wp-content/uploads/2015/11/Weixiang-You-Wenhao-Wu-web.jpg" alt="Weixiang You of Sichuan Express markets to international students from China. Wenhao Wu | Staff Photographer" width="1024" height="683" /> Weixiang You of Sichuan Gourmet markets to international students from China. Wenhao Wu | Staff Photographer
+<p class="p1">Although his restaurant is popular among Oakland’s Chinese population ready for the fiery dish, Weixiang You said he does not see enough non-Chinese customers.</p>
+<p class="p1">These customers, according to You, cannot differentiate authentic Chinese from stereotypical American Chinese dishes like General Tso’s Chicken and Beef Lo Mein.</p>
+<p class="p1">“We serve the authentic Sichuan cuisine. But, to non-Chinese, they are all just Chinese food,” You said.</p>
+<p class="p1">You’s motivation for introducing authentic Sichuan cuisine to Pittsburgh started simply because he missed it himself.</p>
+<p class="p1">“When I first came to Pittsburgh while my wife was a researcher at Pitt, we were yearning for authentic Sichuan food every day,” You said.</p>
+<p class="p1">So rather than waiting for quality Sichuan cuisine to arrive, You and a few Pitt alumni started a Sichuan restaurant of their own six years ago.</p>
+<p class="p1">“There are international Chinese students out there who are just like us back then, longing for quality Chinese food,” You said. “So I believe we have a market. We have international students literally cry eating our food because [it] reminds them of home.”</p>
+<p class="templateTitle"><strong>The Competition</strong></p>
+<p class="p1">A throng of college students flooded into Peter’s Pub across Oakland Avenue for a 21st birthday as Mike Lin pulled out his phone and sent a few messages to his family. Around 11 p.m., Lin was celebrating the end of another 12-hour workday at Szechuan Express.</p>
+<p class="p1">After 20 years of success at his Asian stir-fry restaurant, 12-hour days are still routine for Lin. His work has allowed him to purchase a few luxury cars, but the job’s demands makes them mostly sit idle.</p>
+<p class="p1">“People thought it would be easy to open a restaurant in Oakland because the demand is here, and they all flocked in,” Lin, 43, said. “However, it is, in fact, really hard because your service has to be fast, you can’t afford to hire too many people and you can’t charge too much.”</p>
+
+
+<img class="wp-image-65704 size-full" src="https://pittnews.com/wp-content/uploads/2015/11/Szechuan-Express-2-Valkyrie-Speaker-web.jpg" alt="Szechuan Express 2-Valkyrie Speaker-web" width="1024" height="683" /> Mike Lin, left, owner of Szechuan Express, checks out a customer at his store. Photo: Valkyrie Speaker / Staff Photographer
+<p class="p1">As students line up at food trucks and order take-out, the owners of Asian restaurants must balance the demand for Americanized cuisine and authentic Chinese food, while also offering speedy service.</p>
+<p class="p1">Tong’s Cuisine on Semple Street, which opened in 2011, was one of the first Asian restaurants to experiment with authentic taste, although its menu is divided into two halves: One half for non-Asians, the other half for Asians and Asian-Americans.</p>
+
+
+<img class="wp-image-65691 size-full" src="https://pittnews.com/wp-content/uploads/2015/11/Tongs-Cuisine-with-Server-web.jpg" alt="Students indulge in Chinese dinner at Tong's Cuisine, where Zhi Feng serves &quot;authentic&quot; and &quot;simple&quot; home-style cooking. Meghan Sunners | Senior Staff Photographer" width="1024" height="684" /> Students indulge in Chinese dinner at Tong's Cuisine, which serves "authentic" and "simple" home-style cooking according to owner, Zhi Feng.<br /> Meghan Sunners | Senior Staff Photographer
+<p class="p1">According to its owner, Zhi Feng, 42, competition among restaurants grows more fierce each year.</p>
+<p class="p1">“There were no buffets 20 years ago. We just had Chinese sit-down restaurants,” Feng said. “Now it is impossible to raise the price of Chinese food.”</p>
+<p class="p1">Feng has only raised the price of the lunch special by $1.05 — from $5.25 to $6.30 — over the past four years, while the cost of beef flank steak almost doubled — from $4 per pound to $7 per pound.</p>
+<p class="p1">“I still couldn’t figure out how Szechuan Express can give you a meal with so much vegetables and so many calories for such a low price,” Musante said.</p>
+<p class="p1">Forgoing authenticity, other restaurants carve out niche markets.</p>
+<p class="p1">Sushi Fuku, which replaced a Quiznos on Oakland Avenue in 2012, offers its “roll it or bowl it” Chipotle model of sushi building, according to general manager Ron Oliver, appeals to students looking for what he calls “fast-casual.”</p>
+<p class="p1">“We are not traditional sushi, that is guaranteed,” Oliver said. “With traditional sushi, it’s not fast, it’s not convenient, it’s not cheap.”</p>
+<p class="p1">Oliver said the reason for Sushi Fuku’s success — it opened a second location on Craig Avenue this year along with Fuku Tea on Forbes Avenue — has been its ability to modernize and appeal to a student audience, exemplified in its latest product, the “sushi burrito.”</p>
+<p class="p1">“You have to add a little variety. If you don’t change, it’ll get stale,” Oliver said.</p>
+<p class="p1">According to Georgia Petropoulos, the executive director of the Oakland Business Improvement District, recent customer trends in Oakland show that Asian cuisine is thriving because it offers fresh ingredients.</p>
+<p class="p1">“Although I don’t know how many Asian restaurants are locally sourced, at least they are definitely not using frozen food,” Petropoulos said.</p>
+<p class="p1">There are plenty of non-Asian restaurants that offer fresh food, Petropoulos said, but they don’t have the variety of Asian menus.</p>
+<p class="p1">“College students are looking for food that is lighter in fat and calories. They like fresh vegetables,” Petropoulos said. “In Asian restaurants, you have a lot of steamed vegetables which fits right in this trend.”</p>
+<p class="p1">Brian Deutsch, who writes for a blog called PennsylvAsia where he chronicles the intersections of Pennsylvania and Asia, said Asian cuisine offers options that meet almost every taste palate.</p>
+<p class="p1">“I see people online complain when yet another Asian place opens in Oakland and Squirrel Hill, but that attitude chooses to ignore the diversity of cultures and foods in the region,” Deutsch, who also works for the English Language Institute, said.</p>
+<p class="p1"><strong>The Future</strong></p>
+<p class="p1">In 2014, the owner of Top Shabu Shabu &amp; Lounge moved into Pizza Sola’s old home on Atwood Street. Five months later, Sichuan Gourmet opened at the old location of India Garden, also on Atwood Street.</p>
+
+
+<img class="wp-image-65690 size-full" src="https://pittnews.com/wp-content/uploads/2015/11/Szichuan-Gourmet-Dining-Room-Wenhao-Wu-web.jpg" alt="Sichuan Gourmet on Atwood, &quot;exclusively prides itself on authentic Chinese dishes such as Fish Filets in Hot Chili Oil and Spicy Beef in Flaming Pan.&quot; Wenhao Wu | Staff Photographer" width="1024" height="683" /> Sichuan Gourmet on Atwood, "exclusively prides itself on authentic Chinese dishes such as Fish Filets in Hot Chili Oil and <br /> Spicy Beef in Flaming Pan." Wenhao Wu | Staff Photographer
+<p class="p1">The building on the corner of Forbes and Oakland has transformed in the past five years from Joe Mama’s, an Italian eatery, to Olio Trattoria, a more upscale Italian dining spot and then into Stack’d Burgers and Beer.</p>
+<p class="p1">Mike Hanley was one of the owners of Joe Mama’s, which was an Oakland favorite from around 2000 until he sold the restaurant in 2013 to Brandon Smith, the manager turned owner of Fuel and Fuddle next door, to focus on his other restaurants.</p>
+<p class="p1">Smith and his business partner, Greg Ripper, who both own Fuel and Fuddle, were trying to make a more upscale date restaurant in Oakland.</p>
+<p class="p1">“On the weekends, we did really well,” Smith said. “On Monday, Tuesday, Wednesday, it wasn’t really doing the volume we wanted.”</p>
+<p class="p1">While owning a business in Oakland is great because of the innovative market, Hanley said college students’ palates and schedules shape the success of businesses.</p>
+<p class="p1">“[Oakland] has and continues to be a great market for the right concepts,” Hanley said. “[The Forbes Avenue] corridor can be dependent on the school calendar.”</p>
+<p class="p1">Smith graduated from Pitt in 2006, when he said there was still a lot of Asian restaurants in Oakland, but bars and nightclubs were more popular than restaurants. Now, Smith said students are ready to spend more money for good food.</p>
+<p class="p1">He doesn’t feel the same sense of competition that some Asian restaurant owners might, because he’s fulfilling a certain niche market — “good sandwiches and good beer.”</p>
+<p class="p1">“There’s enough people in Oakland for everyone to serve,” Smith said, who added that the opening of Stack’d has actually brought more customers into Fuel and Fuddle. “The more places we have in Oakland, the better for Oakland.”</p>
+<p class="p1">Even with 20 Asian restaurants already in place, You also predicts the number will continue to grow.</p>
+<p class="p1">“The market is definitely not saturated yet,” You said.</p>
+<p class="p1">Bubble tea, a rarity in Oakland just three years ago, is now a staple at a dozen Asian restaurants in the area. Sushi Fuku opened a bubble tea place on Forbes Avenue around a month ago called Fuku Tea. Chick’n Bubbly runs its two bubble tea machines, perched on the wall, constantly.</p>
+
+
+<img class="wp-image-65702 size-full" src="https://pittnews.com/wp-content/uploads/2015/11/Choi-Oishii-web.jpg" alt="Si Hyuk Choi, owner of Oishii and Chick'n Bubbly, has served Asian cuisine on Oakland Ave. for 10 years. Nikki Moriello | Visual Editor" width="1024" height="684" /> Si Hyuk Choi, owner of Oishii and Chick'n Bubbly, has served Asian cuisine on Oakland Ave. for 10 years.<br /> Nikki Moriello | Visual Editor
+<p class="p1">Sihyuk Choi, the owner of Chick’n Bubbly and Oishii Bento, compared the bubble tea trend to the frozen yogurt craze in Korea. He is worried that bubble tea may be popular right now, but could dry up like frozen yogurt has in Korea.</p>
+<p class="p1">Like Sushi Fuku, Choi has taken some cues from the fast food franchise, to make things “simple and easy.”</p>
+<p class="p1">“If you look at McDonald’s or Starbucks, they trained their workers in a [systematic way],” Choi said. “And after a month, all their workers can cook a burger or something like that. But with a traditional Asian restaurant, we have to hire a chef.”</p>
+<p class="p1">So some Asian restaurateurs move away from the traditional and venture towards innovation.</p>
+<p class="p1">Ren Jiang, 35, the owner of Top Shabu Shabu and Lounge on Atwood Street, said the traditional stir-fry dish is broken, and the industry needs to innovate. Jiang said his restaurant, which specializes in hot pot and Korean barbecue, lets customers cook their own food in a personal hot pot or on the grill rack.</p>
+<p class="p1">And it’s Jiang’s willingness to bring a new style of food to a college neighborhood that drives competition and keeps Asian culture alive.</p>
+<p class="p1">“We are not just selling dishes. We are also selling packages of culinary cultures,” Jiang said.</p>
+<p class="p1"><em>Elizabeth Lepro contributed to this story. </em></p>
+
+<?php include 'footer.php'; ?>
