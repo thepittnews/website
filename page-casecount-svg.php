3fc4cdfc2de38e80ee3559b5c9d36497a6d138ae
Jon Moss <me@jonathanmoss.me>
2021-11-14 18:41:59 -0500

    Remove vaccination info from COVID graphic
    Will be defunct as of Dec. 6, when a mandate takes effect across campus.

--- page-casecount-svg.php
+++ page-casecount-svg.php
@@ -11,7 +11,7 @@ $csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTwWCQl2QwJyfaIBM3x99
 $svgHeader = '<?xml version="1.0" standalone="no"?>
   <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
   "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-  <svg width="320" height="150" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <svg width="320" height="120" version="1.1" xmlns="http://www.w3.org/2000/svg">
     <style>
       @import(<![CDATA["https://fonts.googleapis.com/css?family=Roboto:400&display=swap&ver=5.4.2"]]>);
       text {
@@ -23,9 +23,9 @@ $svgHeader = '<?xml version="1.0" standalone="no"?>
       }
     </style>
 
-    <rect width="320" height="150" style="fill: white; stroke-width: 5; stroke: rgb(0,0,0)" /> <!-- rgb(0,0,255); -->
+    <rect width="320" height="120" style="fill: white; stroke-width: 5; stroke: rgb(0,0,0)" /> <!-- rgb(0,0,255); -->
     <a href="https://pittnews.com/covid19"  target="_blank">
-      <text x="50%" y="15%" font-size="1.4em">Pitt Community COVID-19 Info</text>
+      <text x="50%" y="25%" font-size="1.4em">Pitt Community COVID-19 Info</text>
     </a>
 ';
 
@@ -33,26 +33,17 @@ $fileContents = file_get_contents($csvUrl);
 $rows = str_getcsv(explode("\r\n", $fileContents)[1]);
 
 echo $svgHeader . '
-    <text x="50%" y="30%" font-size="1.1em">Cases as of ' . $rows[0] . '</text>
-    <text x="55%" y="40%" font-size="0.9em">Employees</text>
-    <text x="85%" y="40%" font-size="0.9em">Students</text>
+    <text x="50%" y="45%" font-size="1.2em">Cases as of ' . $rows[0] . '</text>
+    <text x="55%" y="65%" font-size="0.9em">Employees</text>
+    <text x="85%" y="65%" font-size="0.9em">Students</text>
 
-    <text x="20%" y="50%" font-size="0.9em">From last report</text>
-    <text x="55%" y="50%" font-size="0.9em">' . $rows[1] . '</text>
-    <text x="85%" y="50%" font-size="0.9em">' . $rows[3] . '</text>
+    <text x="20%" y="80%" font-size="0.9em">From last report</text>
+    <text x="55%" y="80%" font-size="0.9em">' . $rows[1] . '</text>
+    <text x="85%" y="80%" font-size="0.9em">' . $rows[3] . '</text>
 
-    <text x="20%" y="60%" font-size="0.9em">Total</text>
-    <text x="55%" y="60%" font-size="0.9em">' . $rows[2] . '</text>
-    <text x="85%" y="60%" font-size="0.9em">' . $rows[4] . '</text>
-
-    <text x="50%" y="75%" font-size="1.1em">Vaccination rates as of ' . $rows[0] . '</text>
-    <text x="20%" y="85%" font-size="0.9em">Faculty</text>
-    <text x="45%" y="85%" font-size="0.9em">Staff</text>
-    <text x="75%" y="85%" font-size="0.9em">UG Students</text>
-
-    <text x="20%" y="95%" font-size="0.9em">' . $rows[5] . '%</text>
-    <text x="46%" y="95%" font-size="0.9em">' . $rows[6] . '%</text>
-    <text x="75%" y="95%" font-size="0.9em">' . $rows[7] . '%</text>
+    <text x="20%" y="90%" font-size="0.9em">Total</text>
+    <text x="55%" y="90%" font-size="0.9em">' . $rows[2] . '</text>
+    <text x="85%" y="90%" font-size="0.9em">' . $rows[4] . '</text>
   </svg>
 ';
 ?>
